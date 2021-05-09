183f10c4a07ec6da96b47d11030a865d9ac48f67
akashd50 <akash.as19@gmail.com>
2021-05-09 15:17:27 -0500

    Refactoring

--- app/src/main/java/com/greymatter/managerio/ui/activities/ActivityViewContact.java
+++ app/src/main/java/com/greymatter/managerio/ui/activities/ActivityViewContact.java
@@ -1,20 +1,17 @@
 package com.greymatter.managerio.ui.activities;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.greymatter.managerio.R;
 import com.greymatter.managerio.db.DBServices;
-import com.greymatter.managerio.db.helpers.ContactsDBHelper;
 import com.greymatter.managerio.objects.Contact;
-import com.greymatter.managerio.ui.contacts.uihelpers.ContactsValidator;
 
 import java.util.List;
 
@@ -22,6 +19,7 @@ public class ActivityViewContact extends AppCompatActivity {
     private TextView initialsView;
     private EditText firstNameField, lastNameField, mobileNoField;
     private FloatingActionButton addTransactionButton;
+    private Dialog addTransactionDialog;
     private long currentContactId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +29,11 @@ public class ActivityViewContact extends AppCompatActivity {
 
         currentContactId = getIntent().getLongExtra("contact_id", -1);
 
-        firstNameField = findViewById(R.id.edit_contact_first_name);
-        lastNameField = findViewById(R.id.edit_contact_last_name);
-        mobileNoField = findViewById(R.id.edit_contact_mobile_no);
-        initialsView = findViewById(R.id.edit_contact_initials);
-        addTransactionButton = findViewById(R.id.add_new_transaction_floating_button);
+        firstNameField = findViewById(R.id.activity_view_contact_first_name);
+        lastNameField = findViewById(R.id.activity_view_contact_last_name);
+        mobileNoField = findViewById(R.id.activity_view_contact_mobile_no);
+        initialsView = findViewById(R.id.activity_view_contact_initials);
+        addTransactionButton = findViewById(R.id.activity_view_contact_add_new_transaction_button);
         addTransactionButton.setOnClickListener(onClickListener);
 
         if (currentContactId != -1) {
@@ -56,12 +54,31 @@ public class ActivityViewContact extends AppCompatActivity {
         @Override
         public void onClick(View view) {
             switch (view.getId()) {
-                case R.id.add_new_transaction_floating_button:
-
+                case R.id.activity_view_contact_add_new_transaction_button:
+                    showAddNewTransactionDialog();
+                    break;
+                case R.id.add_transaction_dialog_done:
+                    //addNewContact();
+                    break;
+                case R.id.add_transaction_dialog_cancel:
+                    addTransactionDialog.dismiss();
+                    break;
+                default:
                     break;
             }
         }
     };
+
+    private void showAddNewTransactionDialog() {
+        addTransactionDialog  = new Dialog(ActivityViewContact.this);
+        addTransactionDialog.setCancelable(false);
+        addTransactionDialog.setContentView(R.layout.add_new_transaction_dialog);
+        addTransactionDialog.findViewById(R.id.add_transaction_dialog_done).setOnClickListener(onClickListener);
+        addTransactionDialog.findViewById(R.id.add_transaction_dialog_cancel).setOnClickListener(onClickListener);
+        addTransactionDialog.create();
+        addTransactionDialog.show();
+    }
+
 
     private void updateInitialsView() {
         List<Contact> queryResult = DBServices.getContactsDBHelper().getWithId((int)currentContactId);
